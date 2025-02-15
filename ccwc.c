fbd9e8a6fdf68e7ad528f5fbeed2123246ccfb1a
eterseis <148352159+eterseis@users.noreply.github.com>
2025-02-15 18:07:10 -0300

    feat: add support to wide character counting

--- ccwc.c
+++ ccwc.c
@@ -1,5 +1,7 @@
+#include <locale.h>
 #include <stdio.h>
 #include <string.h>
+#include <wchar.h>
 
 int indexOf(char *array[], int length, char value[]) {
   for (int i = 1; i < length; i++) {
@@ -20,11 +22,10 @@ int getFile(char *array[], int length) {
 }
 
 int countBytes(FILE *file_pointer) {
-  int count = -1;
-  do {
+  int count = 0;
+  while (fgetc(file_pointer) != EOF) {
     count++;
-  } while (fgetc(file_pointer) != EOF);
-  rewind(file_pointer);
+  }
   return count;
 }
 
@@ -36,7 +37,6 @@ int countLines(FILE *file_pointer) {
       count++;
     }
   }
-  rewind(file_pointer);
   return count;
 }
 
@@ -47,7 +47,15 @@ int countWords(FILE *file_pointer) {
     count++;
   }
 
-  rewind(file_pointer);
+  return count;
+}
+
+int countChars(FILE *file_pointer) {
+  int count = 0;
+  wint_t wc;
+  while ((wc = fgetwc(file_pointer)) != WEOF) {
+    count++;
+  }
   return count;
 }
 
@@ -55,18 +63,28 @@ void printOptions(char *array[], int array_length, FILE *f_ptr) {
   printf("%c", 0x20);
   if (indexOf(array, array_length, "-l") != -1) {
     printf("%d ", countLines(f_ptr));
+    fseek(f_ptr, 0, SEEK_SET);
   }
   if (indexOf(array, array_length, "-w") != -1) {
     printf("%d ", countWords(f_ptr));
+    fseek(f_ptr, 0, SEEK_SET);
   }
   if (indexOf(array, array_length, "-c") != -1) {
     printf("%d ", countBytes(f_ptr));
+    fseek(f_ptr, 0, SEEK_SET);
+  }
+  if (indexOf(array, array_length, "-m") != -1) {
+    fclose(f_ptr);
+    f_ptr = fopen(array[getFile(array, array_length)], "rb");
+    printf("%d ", countChars(f_ptr));
+    fseek(f_ptr, 0, SEEK_SET);
   }
 }
 
 int main(int argv_length, char *argv[]) {
+  setlocale(LC_CTYPE, "");
   FILE *file_pointer;
-  file_pointer = fopen(argv[getFile(argv, argv_length)], "r");
+  file_pointer = fopen(argv[getFile(argv, argv_length)], "rb");
 
   if (file_pointer == NULL) {
     return 6;
